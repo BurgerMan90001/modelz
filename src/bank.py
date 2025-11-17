1da9b7ecaf31409a1166f2ebcda03102c448326e
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-11-16 21:28:07 -0600

    add shill_bidding.py

---
+++ src/bank.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+bank_data = pd.read_csv("data/bank-full.csv")
+
+# Input data
+X = bank_data
+
+# Target data
+Y = bank_data
+
+X_train,X_valid, Y_train, Y_valid  = train_test_split(X, Y)
+
+
+#bank_data.describe()
+
+
+
+
--- src/shill_bidding.py
+++ src/shill_bidding.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 
 from sklearn.ensemble import RandomForestRegressor
@@ -8,15 +9,4 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 
-bank_data = pd.read_csv("modelz/data/bank-full.csv")
-
-
-X = bank_data
-
-y = bank_data
-X_train = train_test_split()
-#bank_data.describe()
-
-
-
-
+shill_bidding_data = pd.read_csv("data/shill-bidding.csv")
