59b6e48c64ea484f30049cf4a5c1b13737e2bfa9
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-11-27 15:49:12 -0600

    testing

---
+++ tests/main.py
@@ -0,0 +1,8 @@
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+#from sklearn.preprocessing import OrdinalEncoder
+test = OneHotEncoder()
