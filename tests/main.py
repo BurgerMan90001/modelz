ebe02e0d09b175d61271ddb0c225ec33e5529098
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-12-08 21:32:45 -0600

    computer vision 1

--- tests/main.py
+++ tests/main.py
@@ -4,6 +4,9 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import keras
+import tensorflow as tf
 #from sklearn.preprocessing import OrdinalEncoder
 test = OneHotEncoder()
 
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
